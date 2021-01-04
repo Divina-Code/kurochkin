206330febe19e05fdec8841bea0a0417a13927a0
wolfs-computer <gameproc.me@mail.ru>
2021-01-04 15:40:04 +0300

    modification telega.py

--- telega.py
+++ telega.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
 import telebot
+from random import randint as rid
 
 bot = telebot.TeleBot('1259165465:AAF0YRiXtF5Rn29H9VgRfELm366qBIb5oXM')
 
+# mods
+
 @bot.message_handler(content_types=['text'])
 def ans_mess(message):
-    print(message.chat.username)
-    print(message)
+    mes = message.text
     user = message.chat.username
     id = message.chat.id
-    bot.send_message(id, f"Hello {user}, you write this - {message.text}") # message.text[::-1],
+    bot.send_message(id, "Enter some word")
+    # bot.send_message(id, f"Hello {user}, you write this - {message.text}") # message.text[::-1]
+    inp = message.text.lower().split()
+    for word in inp:
+        if word[::-1] == word:
+            bot.send_message(id, "that word is polindrom")
+        else:
+            bot.send_message(id, "that word isn`t polindrom")
+
 
 bot.polling() # забираем сообщение у бота
